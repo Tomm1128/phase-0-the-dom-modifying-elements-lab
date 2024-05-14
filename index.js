f9e036ea96313c357a621c245c77ef4d2364e9ea
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-14 10:48:03 -0400

    Remove main tag and add h1 with text

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.getElementById("main")
+main.remove()
+
+const body = document.getElementsByTagName("body")
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent = "TJ is the champion"
+document.body.append(newHeader)
